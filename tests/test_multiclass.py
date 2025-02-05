9fd92209b7295cd9c6b6c0a00b326dc417b5876d
Pierre Nodet <pierre.nodet@orange.com>
2025-02-05 11:50:32 +0100

    remove logistic regression ovr warnings

--- tests/test_multiclass.py
+++ tests/test_multiclass.py
@@ -18,17 +18,12 @@ def test_weighted_ovr_with_noweights_equals_ovr(n_classes):
         random_state=seed,
     )
 
-    base = LogisticRegression(multi_class="ovr", random_state=seed)
     ovr = OneVsRestClassifier(LogisticRegression(random_state=seed))
     wovr = WeightedOneVsRestClassifier(LogisticRegression(random_state=seed))
 
-    base.fit(X, y)
     ovr.fit(X, y)
     wovr.fit(X, y)
 
-    assert_array_equal(base.predict(X), ovr.predict(X))
     assert_array_equal(ovr.predict(X), wovr.predict(X))
-    assert_array_almost_equal(base.predict_proba(X), ovr.predict_proba(X))
     assert_array_almost_equal(ovr.predict_proba(X), wovr.predict_proba(X))
-    assert_array_almost_equal(base.decision_function(X), ovr.decision_function(X))
     assert_array_almost_equal(ovr.decision_function(X), wovr.decision_function(X))
